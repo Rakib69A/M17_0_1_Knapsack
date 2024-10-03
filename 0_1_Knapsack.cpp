21e0be3a8afa3660c6deba26c56fc1bf816cb1d9
Rakib <rakibhasan75089@gmail.com>
2024-10-03 20:13:29 +0600

    0-1 kanpsack bottom up approach

--- 0_1_Knapsack.cpp
+++ 0_1_Knapsack.cpp
@@ -1,21 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-int Knapsack(int n,int weight[],int value[],int W){
-    if(weight[n] <= W){
-        //duita opion
-        
+const int maxN = 1000;
+const int maxW = 1000;
+int dp[maxN][maxW];
+int knapsack(int n, int weight[], int value[],int W){
+    if(n==0 || W==0)
+        return 0;
+    if(dp[n][W] != -1){
+        return dp[n][W];
+    }
+    if(weight[n-1] <= W){
+        //Two option
+        //Option 1 : niye dekhbo
+        int op1 = knapsack(n-1, weight, value, W - weight[n-1]) + value[n-1];
+        //Option 2 : nibo na
+        int op2 = knapsack(n-1, weight, value, W);
+
+        return dp[n][W] = max(op1,op2);
+    }
+    else{
+        int op2 = knapsack(n-1, weight, value, W);
+        return dp[n][W] = op2;
     }
 }
 int main()
 {
     int n;
-    int weight[n],value[n];
+    cin>>n;
+    int weight[n], value[n];
     for(int i=0;i<n;i++)
         cin>>weight[i];
     for(int i=0;i<n;i++)
         cin>>value[i];
-    int W;
-    cin>>W;
-    cout<<Knapsack(n-1,weight,value,W)<<endl;
+    int W;cin>>W;
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=W;j++){
+            dp[i][j] = -1;
+        }
+    }
+    cout<<knapsack(n,weight,value,W)<<endl;
     return 0;
 }
